f5bcb72951d1988cf1f032f73d8cbe206b025e30
Josef Reidinger <jreidinger@suse.cz>
2024-11-06 20:25:39 +0100

    list of known repos

--- main.c
+++ main.c
@@ -2,7 +2,12 @@
 #include "lib.h"
 
 int main() {
-   printf("Hello, World!");
+   printf("List of repos:\n");
    init_target("/");
+   struct RepositoryList list = list_repositories();
+   for (unsigned i = 0; i < list.size; ++i){
+      struct Repository* repo = list.repos + i;
+      printf("repo %i: %s\n", i, repo->userName);
+   }
    return 0;
 }
