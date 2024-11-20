be5b40458c3878cc0fc5a02768f7bacaab4968e3
Martin Vidner <mvidner@suse.com>
2024-11-20 11:53:47 +0100

    pass zypp root to example program

--- rust/zypp-agama-example/src/main.rs
+++ rust/zypp-agama-example/src/main.rs
@@ -1,14 +1,15 @@
+use std::env;
+
 fn main() {
-    println!("Example failure of initialize:");
-    let result = zypp_agama::init_target("/bla", |_,_,_| {
-    });
-    if let Err(error) = result {
-        println!("Failed to initialize target: {}", error);
-        println!("Lets try some real work");
-        println!("");
-    };
+    println!("Usage: main [ROOT]");
+    println!("  ROOT defaults to /");
+
+    let args: Vec<String> = env::args().collect();
+    let default_root = "/".to_owned();
+    let root = args.get(1).unwrap_or(&default_root);
+
     println!("List of repositories:");
-    let result = zypp_agama::init_target("/", |text, step, total| {
+    let result = zypp_agama::init_target(root, |text, step, total| {
         println!("Initializing target: {}/{} - {}", step, total, text)
     });
     if let Err(error) = result {
