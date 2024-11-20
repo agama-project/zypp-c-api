ba464eb6c6664a873629f47f41563e4765c816c8
Martin Vidner <mvidner@suse.com>
2024-11-20 13:32:44 +0100

    reformat C API for Doxygen, fill in details

--- c-layer/include/lib.h
+++ c-layer/include/lib.h
@@ -7,45 +7,59 @@
 extern "C" {
 #endif
 struct Repository {
-  char *url;        // owned
-  char *alias;      // owned
-  char *userName;   // owned
+  char *url;        ///< owned
+  char *alias;      ///< owned
+  char *userName;   ///< owned
 };
 
 struct RepositoryList {
   const unsigned size;
-  // dynamic array with given size
-  struct Repository *repos; // owned, *size* items
+  /// dynamic array with given size
+  struct Repository *repos; ///< owned, *size* items
 };
 
-// status struct to pass and obtain from calls that can fail.
-// After usage free with free_status function
+/// status struct to pass and obtain from calls that can fail.
+/// After usage free with \ref free_status function.
+///
+/// Most functions act as *constructors* for this, taking a pointer
+/// to it as an output parameter, disregarding the struct current contents
+/// and filling it in. Thus, if you reuse a `Status` without \ref free_status
+/// in between, `error` will leak.
 struct Status {
   // lets use enum for future better distinguish
   enum STATE {
     STATE_SUCCEED,
     STATE_FAILED,
   } state;
-  // detailed user error what happens. Only defined when not succeed
-  char *error; // owned
+  /// detailed user error what happens. Only defined when not succeed
+  char *error; ///< owned
 };
 void free_status(struct Status s);
 
-// Progress reporting callback used by methods that takes longer.
-// text is text for user describing what is happening now
-// stage is current stage number starting with 0
-// total is count of stages. It should not change during single call of method.
-// user_data is never touched by method and is used only to pass local data for callback
-// TODO: Do we want to support response for callback that allows early exit of execution?
+/// Progress reporting callback used by methods that takes longer.
+/// @param text  text for user describing what is happening now
+/// @param stage current stage number starting with 0
+/// @param total count of stages. It should not change during single call of method.
+/// @param user_data is never touched by method and is used only to pass local data for callback
+/// @todo Do we want to support response for callback that allows early exit of execution?
 typedef void (*ProgressCallback)(const char *text, unsigned stage, unsigned total, void *user_data);
+/// Initialize Zypp target (where to install packages to)
+/// @param root
+/// @param[out] status
+/// @param progress
+/// @param user_data
 void init_target(const char *root, struct Status *status, ProgressCallback progress, void *user_data);
 
-// repository array in list
-// when no longer needed, use free_repository_list to release memory
+/// repository array in list.
+/// when no longer needed, use \ref free_repository_list to release memory
 struct RepositoryList list_repositories();
 
 void free_repository_list(struct RepositoryList *repo_list);
 
+///
+/// @param[out] status (will overwrite existing contents)
+/// @param callback
+/// @param data
 void refresh_repositories(struct Status *status, ZyppProgressCallback callback, void *data);
 
 // the last call that will free all pointers to zypp holded by agama
