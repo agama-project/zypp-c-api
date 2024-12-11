90de08ed45f489ca7992721f0c09a71c6fcf84d5
Imobach González Sosa <igonzalezsosa@suse.com>
2024-12-11 14:24:36 +0000

    fix: detect rustc-link-search using CARGO_MANIFEST_DIR

--- rust/zypp-agama-sys/build.rs
+++ rust/zypp-agama-sys/build.rs
@@ -1,5 +1,11 @@
+use std::{env, path::Path};
+
 fn main() {
-    println!("cargo::rustc-link-search=native=../c-layer/");
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    println!(
+        "cargo::rustc-link-search=native={}",
+        Path::new(&manifest_dir).join("../../c-layer").display()
+    );
     println!("cargo::rustc-link-lib=static=agama-zypp");
     println!("cargo::rustc-link-lib=dylib=zypp");
     // NOTE: install the matching library for your compiler version, for example
